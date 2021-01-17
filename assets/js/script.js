e64d2a5e00a78a6ef0d10be83b1abcb584007584
jennifer gomez <jennifer23gomez7@gmail.com>
2021-01-16 23:52:16 -0800

    drag and drop zones added

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,6 +188,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     console.dir(taskListEl);
   }
 };
@@ -208,7 +209,15 @@ var dropTaskHandler = function(event) {
     else if (statusType === "tasks-completed") {
       statusSelectEl.selectedIndex = 2;
     }
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
+};
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
 };
 
 // Create a new task
@@ -224,3 +233,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
